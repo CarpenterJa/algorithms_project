31332371d31712e7c26811eaca52ec0b945cffca
anthonymonti2 <anthonymonti2@gmail.com>
2021-03-30 22:04:14 -0500

    Added our mock grocery store

--- main.py
+++ main.py
@@ -94,17 +94,50 @@ class Graph:
             print("%d -- %d == %d" % (u, v, weight))
         print("Minimum Spanning Tree" , minimumCost)
  
-# #Driver code
-# g = Graph(4)
-# g.addEdge(0, 1, 10)
-# g.addEdge(0, 2, 6)
-# g.addEdge(0, 3, 5)
-# g.addEdge(1, 3, 15)
-# g.addEdge(2, 3, 4)
  
-totalNodes = 8
+totalNodes = 14
 g = Graph(totalNodes)
 
+# Starting node
+g.addEdge(0,1,20)
+
+# Add links for node 1
+g.addEdge(1,13,15)
+g.addEdge(1,2,15)
+g.addEdge(1,5,10)
+
+# Add links for node 2
+g.addEdge(2,6,10)
+g.addEdge(2,3,15)
+
+# Add links for node 3
+g.addEdge(3,4,15)
+g.addEdge(3,7,10)
+
+# Add links for node 4
+g.addEdge(4,8,10)
+
+# Add links for node 5
+g.addEdge(5,9,10)
+
+# Add links for node 6
+g.addEdge(6,10,10)
+
+# Add links for node 7
+g.addEdge(7,11,10)
+
+# Add links for node 8
+g.addEdge(8,12, 10)
+
+# Add links for node 9
+g.addEdge(9,13,20)
+g.addEdge(9,10,15)
+
+# Add links for node 10
+g.addEdge(10,11,15)
+
+# Add links for node 11
+g.addEdge(11,12,15)
 
 # Function call
 g.KruskalMST()
