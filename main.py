62ba596a4efa15c909c75582020009422217c513
anthonymonti2 <anthonymonti2@gmail.com>
2021-03-31 21:43:48 -0500

    Simple Floyd algo working

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from collections import defaultdict
- 
+import math
+from enum import Enum
 # Class to represent a graph
  
 # Graph class comes from https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-algorithm-greedy-algo-2/
 class Graph:
- 
+    class GraphEnums(Enum):
+        VERTEX = 0
+        LINK = 1
+        WEIGHT = 2
+
     def __init__(self, vertices):
         self.V = vertices  # No. of vertices
         self.graph = []  # default dictionary
@@ -20,6 +25,9 @@ class Graph:
         if parent[i] == i:
             return i
         return self.find(parent, parent[i])
+
+    def getNumVerticies(self):
+        return self.V
  
     # A function that does union of two sets of x and y
     # (uses union by rank)
@@ -94,50 +102,123 @@ class Graph:
             print("%d -- %d == %d" % (u, v, weight))
         print("Minimum Spanning Tree" , minimumCost)
  
- 
-totalNodes = 14
-g = Graph(totalNodes)
+class FloydWarshallTraverisal():
+    distances = None
+    nextVert = None
 
-# Starting node
-g.addEdge(0,1,20)
+    def __init__(self, numVerticies):
+        #numVerticies = numVerticies - 1
 
-# Add links for node 1
-g.addEdge(1,13,15)
-g.addEdge(1,2,15)
-g.addEdge(1,5,10)
+        # https://stackoverflow.com/questions/10712002/create-an-empty-list-in-python-with-certain-size
+        # Make array to hold all the minimum distances
+        # Make single array with numVerticies indices
+        self.distances = list(range(numVerticies))
+        for i in range(numVerticies):
+            # Each index holds another array making it a 2d array
+            self.distances[i] = list(range(numVerticies))
+            for q in range(numVerticies):
+                self.distances[i][q] = math.inf
 
-# Add links for node 2
-g.addEdge(2,6,10)
-g.addEdge(2,3,15)
+        # Make array to hold vertex indicies
+        self.nextVert = list(range(numVerticies))
+        for i in range(numVerticies):
+            self.nextVert[i] = list(range(numVerticies))
+            for q in range(numVerticies):
+                self.nextVert[i][q] = None
 
-# Add links for node 3
-g.addEdge(3,4,15)
-g.addEdge(3,7,10)
+    def findShortestPairs(self, graph):
+        print(Graph.GraphEnums.VERTEX.value)
+        for edge in graph.graph:
+            u = edge[Graph.GraphEnums.VERTEX.value]
+            v = edge[Graph.GraphEnums.LINK.value]
+            self.distances[u][v] = edge[Graph.GraphEnums.WEIGHT.value]
+            self.nextVert[u][v] = v
 
-# Add links for node 4
-g.addEdge(4,8,10)
+        for vertex in range(graph.V):
+            self.distances[vertex][vertex] = 0
+            self.nextVert[vertex][vertex] = vertex
 
-# Add links for node 5
-g.addEdge(5,9,10)
+        for k in range(graph.V):
+            for i in range(graph.V):
+                for j in range(graph.V):
+                    distSum = (self.distances[i][k] + self.distances[k][j])
+                    if self.distances[i][j] > distSum:
+                        self.distances[i][j] = distSum
+                        self.nextVert[i][j] = self.nextVert[i][k]
+    
+    def findShortestPath(self, numVerticies):
+        print("Following matrix shows the shortest distances between every pair of vertices")
+        for i in range(numVerticies):
+            for j in range(numVerticies):
+                if(self.distances[i][j] == math.inf):
+                    # Print without new line
+                    print("%8s" % ("INF"),end='')
+                else:
+                    print("%7d\t" % (self.distances[i][j]), end='')
+            print("")       
+        
+        # u = edge[Graph.GraphEnums.VERTEX]
+        # v = edge[Graph.GraphEnums.LINK]
+        # if self.nextVert[u][v] == None:
+        #     return None
+        
+        # path = [u]
+        # while not(u == v):
+        #     u = self.nextVert[u][v]
+        #     path.append(u)
+        # return path
 
-# Add links for node 6
-g.addEdge(6,10,10)
+#------------------------------------------------------------------------------        
+# Only run code if main called this file
+if __name__ == "__main__":
+    totalNodes = 14
+    g = Graph(totalNodes)
 
-# Add links for node 7
-g.addEdge(7,11,10)
+    # Starting node
+    g.addEdge(0,1,20)
 
-# Add links for node 8
-g.addEdge(8,12, 10)
+    # Add links for node 1
+    g.addEdge(1,13,15)
+    g.addEdge(1,2,15)
+    g.addEdge(1,5,10)
 
-# Add links for node 9
-g.addEdge(9,13,20)
-g.addEdge(9,10,15)
+    # Add links for node 2
+    g.addEdge(2,6,10)
+    g.addEdge(2,3,15)
 
-# Add links for node 10
-g.addEdge(10,11,15)
+    # Add links for node 3
+    g.addEdge(3,4,15)
+    g.addEdge(3,7,10)
 
-# Add links for node 11
-g.addEdge(11,12,15)
+    # Add links for node 4
+    g.addEdge(4,8,10)
 
-# Function call
-g.KruskalMST()
+    # Add links for node 5
+    g.addEdge(5,9,10)
+
+    # Add links for node 6
+    g.addEdge(6,10,10)
+
+    # Add links for node 7
+    g.addEdge(7,11,10)
+
+    # Add links for node 8
+    g.addEdge(8,12, 10)
+
+    # Add links for node 9
+    g.addEdge(9,13,20)
+    g.addEdge(9,10,15)
+
+    # Add links for node 10
+    g.addEdge(10,11,15)
+
+    # Add links for node 11
+    g.addEdge(11,12,15)
+
+    FWTraversial = FloydWarshallTraverisal(g.getNumVerticies())
+
+    # Find all the shortest pairs
+    FWTraversial.findShortestPairs(g)
+
+    # Find the shortest path given a starting index
+    FWTraversial.findShortestPath(g.getNumVerticies())
