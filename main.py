e9a7a0ad10392e5b2fb6c5ffdcc7974d996ad080
anthonymonti2 <anthonymonti2@gmail.com>
2021-04-30 14:45:05 -0500

    Working on setting up the grocery store from the config file

--- main.py
+++ main.py
@@ -2,9 +2,127 @@ from collections import defaultdict
 from Graph import Graph
 from FloydWarshallTraversial import FloydWarshallTraverisal
 
+
+def ConfigGroceryStore(fileName):
+    # Read in the config file
+    # Generate the grocery store from the file
+
+    # Try to open the file
+    with open(fileName) as f:
+        # Read until we hit no white space
+        endOfFile = False
+        while not endOfFile:
+            newLine = f.readline()
+            if len(newLine) == 0:
+                endOfFile = True
+            elif newLine[0:2] == "//":
+                # Line is a comment so skip it
+                continue
+            elif len(newLine) > 0 and newLine == '\n':
+                # Line is blank so skip it
+                continue
+            else:
+                # Line is real text so decode it
+                # The line contains escape a new line char so remove it
+                newLine = newLine.replace('\n','')
+                print(newLine)
+                if newLine[0] == 'V':
+                    # This line has the vertex number in it
+                    # This will throw an error if  there is more than just a number on this line
+                    vertexNum = int(newLine[1:])
+                elif "Type:" in newLine:
+                    # This line contains the category of food
+                    foodType = ExtractVertexType("Type:",newLine)
+                elif "Food:" in newLine:
+                    # This line contains the food at this node
+                    listOfFoods = ExtractVertexFood("Food:",newLine)
+                elif "Edges:" in newLine:
+                    # This line contains all the edges for this node
+                    vertexEdges = ExtractEdges("Edges:",newLine)
+                else:
+                    # Unknown line so throw an error
+                    pass
+                
+def ExtractVertexType(configId,configText):
+    # Remove Type: from the line
+    return configText.replace(configId,'')
+
+def ExtractVertexFood(configId,configText):
+    # Remove the configId from the line
+    listOfFoodsStr = configText.replace(configId,'')
+
+    # Remove any spaces
+    listOfFoodsStr = listOfFoodsStr.replace(' ','')
+
+    # Check if the line is equal to "None" which means there are no
+    # items at this node. Probably a register or entrance/exit
+    if(listOfFoodsStr == "None"):
+        return None
+    else:
+        # Split line into a list
+        listOfFoods = listOfFoodsStr.split(',')
+        return listOfFoods
+
+def ExtractEdges(configId,configText):
+    # Remove the configId from the line
+    listOfEdgesStr = configText.replace(configId,'')
+
+    # Remove any space
+    listOfEdgesStr = listOfEdgesStr.replace(' ','')
+
+    # Split line into array
+    listOfEdges = listOfEdgesStr.split(',')
+
+    # Take the list of each edge and weight and convert the items to number
+    # Then create a list of those edges and weights
+    listOfEdgesNum = list()
+    for edge in listOfEdges:
+        foundStart = False
+        foundEnd = False
+        foundEdge = False
+        foundWeight = False
+        tempEdgeWeightPair = list()
+        numAsStr = ""
+        while len(edge) > 0 and foundEnd == False:
+            # Find the start character of '['
+            if not foundStart and edge[0] == "[":
+                foundStart = True
+            elif foundStart and edge[0] == "]":
+                # Found the end of the edge weight pair
+                foundEnd = True
+            elif foundStart:
+                if edge[0].isdigit():
+                    # This char is a digit so add it to the value to convert
+                    numAsStr += edge[0]
+                else:
+                    # This char is not a digit so we should convert
+                    if foundEdge:
+                        # Convert the number and add it to the edge slot
+                        tempEdgeWeightPair.append(int(numAsStr))
+                        foundEdge = True
+                        numAsStr = ''
+                    elif foundWeight:
+                        # Conver the number and add it to the weight slot
+                        tempEdgeWeightPair.append(int(numAsStr))
+                        foundWeight = True
+                        numAsStr = ''
+                    else:
+                        #Error
+                        pass
+            # Remove the first char from the string after each iteration
+            edge = edge[1:]
+
+        # Once out of the loop we can assemble the edge weight pair into the overall list
+        listOfEdgesNum.append(tempEdgeWeightPair)
+        
+    return listOfEdgesNum
+
+
 #------------------------------------------------------------------------------        
 # Only run code if main called this file
 if __name__ == "__main__":
+    ConfigGroceryStore("groceryStoreConfig.txt")
+
     totalNodes = 14
     g = Graph(totalNodes)
 
